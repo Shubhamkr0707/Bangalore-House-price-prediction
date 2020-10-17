fb8c8ebfbe7015c3a2759ffba119c3b83ec8250f
Shubhamkr0707 <49731798+Shubhamkr0707@users.noreply.github.com>
2020-10-17 11:26:23 +0530

    House price prediction

---
+++ house price/Modal/projectmodal.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np 
+from matplotlib import pyplot as plt
+# %matplotlib inline
+# matplotlib.rcParms["figure.figure"]=(20,10)
+df1=pd.read_csv("D:\work\Data science\Project House prediction price\Modal\bengaluru_house_prices.csv")
+print(df1.head())
---
+++ house price/readme.txt
@@ -0,0 +1,5 @@
+Note:-This project is based on Bangalore Home price prediction.
+1.It is based on Ui,we have to choose bhk,bath,area and loaction,
+on all these factor it will calculate the price.
+2.To run this project kindly install requirement.txt
+3.It will work on local server.
